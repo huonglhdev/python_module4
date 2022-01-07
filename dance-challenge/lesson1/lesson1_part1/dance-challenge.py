deb4e39315d949d70af089f10fbef4c40533440d
Lâm Hậu Huống <huonglh.dev@gmail.com>
2022-01-08 00:34:41 +0700

    Pygame

---
+++ dance-challenge/lesson1/lesson1_part1/dance-challenge.py
@@ -0,0 +1,33 @@
+from random import randint
+import pgzrun
+
+WIDTH = 800
+HEIGHT = 600
+CENTER_X = WIDTH / 2
+CENTER_Y = HEIGHT / 2
+
+move_list = []
+display_list = []
+
+score = 0
+current_move = 0
+count = 4
+dance_length = 4
+
+say_dance = False
+show_countdown = True
+moves_complete = False
+game_over = False
+
+dancer = Actor("dancer-start")
+dancer.pos = CENTER_X + 5, CENTER_Y - 40
+up = Actor("up")
+up.pos = CENTER_X, CENTER_Y + 110
+right = Actor("right")
+right.pos = CENTER_X + 60, CENTER_Y + 170
+down = Actor("down")
+down.pos = CENTER_X, CENTER_Y + 230
+left = Actor("left")
+left.pos = CENTER_X - 60, CENTER_Y + 170
+
+pgzrun.go()
---
+++ dance-challenge/lesson2/lesson2_part2/dance-challenge.py
@@ -0,0 +1,93 @@
+from random import randint
+import pgzrun
+
+WIDTH = 800
+HEIGHT = 600
+CENTER_X = WIDTH / 2
+CENTER_Y = HEIGHT / 2
+
+move_list = []
+display_list = []
+
+score = 0
+current_move = 0
+count = 4
+dance_length = 4
+
+say_dance = False
+show_countdown = True
+moves_complete = False
+game_over = False
+
+dancer = Actor("dancer-start")
+dancer.pos = CENTER_X + 5, CENTER_Y - 40
+up = Actor("up")
+up.pos = CENTER_X, CENTER_Y + 110
+right = Actor("right")
+right.pos = CENTER_X + 60, CENTER_Y + 170
+down = Actor("down")
+down.pos = CENTER_X, CENTER_Y + 230
+left = Actor("left")
+left.pos = CENTER_X - 60, CENTER_Y + 170
+
+def draw():
+    global game_over, score, say_dance
+    global count, show_countdown
+    if not game_over:
+        screen.clear()
+        screen.blit("stage", (0, 0))
+        dancer.draw()
+        up.draw()
+        down.draw()
+        right.draw()
+        left.draw()
+        screen.draw.text("Score: " +
+                        str(score), color="black",
+                        topleft=(10, 10))
+        if say_dance:
+            screen.draw.text("Dance!", color="black",
+                            topleft=(CENTER_X - 65, 150), fontsize=60)
+        if show_countdown:
+            screen.draw.text(str(count), color="black",
+                            topleft=(CENTER_X - 8, 150), fontsize=60)
+    else:
+        screen.clear()
+        screen.blit("stage", (0, 0))
+        screen.draw.text("Score: " +
+                        str(score), color="black",
+                        topleft=(10, 10))
+        screen.draw.text("GAME OVER!", color="black",
+                        topleft=(CENTER_X - 130, 220), fontsize=60)
+    return
+
+
+def update_dancer(move):
+    global game_over
+    if not game_over:
+        if move == 0:
+            up.image = "up-lit"
+            dancer.image = "dancer-up"
+        elif move == 1:
+            right.image = "right-lit"
+            dancer.image = "dancer-right"
+        elif move == 2:
+            down.image = "down-lit"
+            dancer.image = "dancer-down"
+        else:
+            left.image = "left-lit"
+            dancer.image = "dancer-left"
+    return
+
+def on_key_up(key):
+    global score, game_over, move_list, current_move
+    if key == keys.UP:
+        update_dancer(0)
+    elif key == keys.RIGHT:
+        update_dancer(1)
+    elif key == keys.DOWN:
+        update_dancer(2)
+    elif key == keys.LEFT:
+        update_dancer(3)
+    return
+
+pgzrun.go()
